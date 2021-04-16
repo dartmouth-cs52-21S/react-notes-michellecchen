5a50cf3c679c8ddbc04e65d3ebb301812a45d0dd
Michelle Chen <michelle.chen.22@dartmouth.edu>
2021-04-16 02:54:34 -0700

    linted

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import $ from 'jquery';
 import './style.scss';
 
 function counter() {
-    let num = 0;
-    setInterval(function() {
-        num++;
-        $('#main').html(`You've been on this page for ${num} seconds. Please do something more worthwhile with your time.`);
-    }, 1000);
+  let num = 0;
+  setInterval(() => {
+    num += 1;
+    $('#main').html(`You've been on this page for ${num} seconds. Please do something more worthwhile with your time.`);
+  }, 1000);
 }
 
 counter();
