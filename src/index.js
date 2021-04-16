30e88e96026d0ea724d846c14caa047e11bdd808
Michelle Chen <michelle.chen.22@dartmouth.edu>
2021-04-16 02:42:57 -0700

    everything!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+function counter() {
+    let num = 0;
+    setInterval(function() {
+        num++;
+        $('#main').html(`You've been on this page for ${num} seconds. Please do something more worthwhile with your time.`);
+    }, 1000);
+}
+
+counter();
